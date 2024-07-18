48916454954f6f8b292a7bcea0a80aa8b5a18fb0
James <r@dical.org>
2024-07-18 21:14:48 +1200

    Update rro-name-checker.sh
    Added some usage and info

--- rro-name-checker.sh
+++ rro-name-checker.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+## Purpose of this script is to take in a space (or comma, or both) separated line of IPs, such as from a Route Record Object in MPLS - and query them for their names. 
+## It could be used as part of a larger tool to help network operators see an MPLS RRO path, for instance. Replace dns_server with your own.
+## Usage rro-name-checker.sh "10.1.1.1 10.20.2.2 10.40.4.4"
+## This will spit out your DNS names (probably for loopbacks or interface IPs, if they're in your DNS!)
 
 # Check if an argument is provided
 if [ -z "$1" ]; then
@@ -7,7 +11,7 @@ if [ -z "$1" ]; then
 fi
 
 # DNS server to use
-dns_server="10.128.15.253"
+dns_server="192.168.0.1" ##example only
 
 # Read the input string of IP addresses and remove all commas
 ip_addresses=$(echo $1 | tr -d ',')
@@ -18,7 +22,7 @@ IFS=' ' read -r -a ip_array <<< "$ip_addresses"
 # Loop through each IP address and perform a DNS query
 for ip in "${ip_array[@]}"; do
   # Perform the DNS query using the specified DNS server
-  name=$(nslookup $ip $dns_server | grep 'name =' | awk '{print $4}' | sed 's/\.mydomain\.net\.$//')
+  name=$(nslookup $ip $dns_server | grep 'name =' | awk '{print $4}' | sed 's/\.mydomain\.net\.$//') ## the final sed is to clean up DNS records to remove trailing TLD etc - edit as you wish 
 
   # Check if a name was found
   if [ -z "$name" ]; then

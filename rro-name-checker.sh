43b221798538ad38625be276655f0449a67e6c7e
James <r@dical.org>
2024-09-19 07:57:09 +1200

    Update rro-name-checker.sh
    Updating to version I had on my desktop

--- rro-name-checker.sh
+++ rro-name-checker.sh
@@ -1,33 +1,63 @@
 #!/bin/bash
-## Purpose of this script is to take in a space (or comma, or both) separated line of IPs, such as from a Route Record Object in MPLS - and query them for their names. 
-## It could be used as part of a larger tool to help network operators see an MPLS RRO path, for instance. Replace dns_server with your own.
-## Usage rro-name-checker.sh "10.1.1.1 10.20.2.2 10.40.4.4"
-## This will spit out your DNS names (probably for loopbacks or interface IPs, if they're in your DNS!)
-
+## Script will take in an RRO/ERO style list of IPs and reverse look em up, then print a little graph 
 # Check if an argument is provided
 if [ -z "$1" ]; then
-  echo "Usage: $0 'ip1 ip2 ip3 ...'"
+  echo "Usage: $0 'ip1(flag=...) ip2(flag=...) ...'"
   exit 1
 fi
 
 # DNS server to use
-dns_server="192.168.0.1" ##example only
+dns_server="192.168.12.1"
 
-# Read the input string of IP addresses and remove all commas
-ip_addresses=$(echo $1 | tr -d ',')
+# Extract only the dotted decimal IP addresses from the input
+ip_addresses=$(echo "$1" | grep -oE '\b([0-9]{1,3}\.){3}[0-9]{1,3}\b')
 
 # Split the string into an array
-IFS=' ' read -r -a ip_array <<< "$ip_addresses"
+IFS=$'\n' read -r -d '' -a ip_array <<< "$ip_addresses"
+
+# Declare an array to store unique hostnames
+unique_hostnames=()
 
 # Loop through each IP address and perform a DNS query
 for ip in "${ip_array[@]}"; do
   # Perform the DNS query using the specified DNS server
-  name=$(nslookup $ip $dns_server | grep 'name =' | awk '{print $4}' | sed 's/\.mydomain\.net\.$//') ## the final sed is to clean up DNS records to remove trailing TLD etc - edit as you wish 
+  name=$(nslookup "$ip" "$dns_server" | grep 'name =' | awk '{print $4}' | sed 's/\.packetfabric\.net\.$//')
 
-  # Check if a name was found
-  if [ -z "$name" ]; then
+  # Extract the important part of the DNS name
+  important_part=$(echo "$name" | grep -oE '[a-z]{3}[0-9]\.[a-z]{3}[0-9]')
+
+  # Check if the important part was found
+  if [ -z "$important_part" ]; then
     echo "$ip: No DNS name found"
   else
-    echo "$ip: $name"
+    echo "$ip: $important_part"
+
+    # Check if the hostname is already in the list
+    if [[ ! " ${unique_hostnames[@]} " =~ " ${important_part} " ]]; then
+      unique_hostnames+=("$important_part")
+    fi
+  fi
+done
+
+# Draw the graph of unique hosts
+echo -e "\nGraph of Unique Hosts:"
+for ((i = 0; i < ${#unique_hostnames[@]}; i++)); do
+  hostname="${unique_hostnames[$i]}"
+
+  # Calculate the number of spaces needed to align the hostname
+  if [ ${#hostname} -eq 7 ]; then
+    padding="    "
+  else
+    padding="   "
+  fi
+
+  echo "+-------------+"
+  echo "| ${hostname}${padding}|"
+  echo "+-------------+"
+
+  # Check if there is a next hostname to connect
+  if [ $i -lt $((${#unique_hostnames[@]} - 1)) ]; then
+    echo "       |"
+    echo "       v"
   fi
 done
